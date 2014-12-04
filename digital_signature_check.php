51560b9e285646e5c6c10c48246503623d71d640
matejbasic <mbasic0@foi.hr>
2014-12-04 08:09:43 +0100

    da

--- digital_signature_check.php
+++ digital_signature_check.php
@@ -13,21 +13,23 @@
 	$data = file_get_contents($data_location);
 	$data_items = explode("-+-", $data);
 	
-	$org_data_path = str_replace("_signed", "", $path);
+	$org_data_location = getenv("DOCUMENT_ROOT") . str_replace("_signed", "", $path);
+	
+	$org_data = file_get_contents($org_data_location);
+	$org_digest = sha1($org_data);
 	
 	
-	$org_data = file_get_contents($org_data_path);
 	$org_data_signed = $data_items[0];
-	
+	$org_signed_digest = sha1($org_data_signed);
 	
 	$public_key_location = getenv("DOCUMENT_ROOT") . "/files/kljucevi/javni_kljuc.txt";
 	$public_key = file_get_contents($public_key_location);
 	$rsa->loadKey($public_key);
 	$decrypted_digest = $rsa->decrypt($data_items[1]);
-	$org_digest = sha1($org_data);
-	$org_signed_digest = sha1($org_data_signed);
 	
-	if ($decrypted_digest == $org_digest && $decrypted_digest == $org_signed_digest) {
+	
+	
+	if ($decrypted_digest == $org_digest) {
 		echo "valid";
 	}
 	else {
