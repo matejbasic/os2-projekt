93ac7411a4907621b79619e3a98ad2e7852490de
matejbasic <mbasic0@foi.hr>
2014-12-10 20:04:32 +0100

    minor fix

--- digital_signature_check.php
+++ digital_signature_check.php
@@ -25,9 +25,12 @@
 		$data = file_get_contents($data_location);
 		$data_items = explode("-- Digital Signature --", $data);
 		
-		// u zadatku pise "...pri cemu do pogreske (promjene sadrzaja) moze doci u originalnoj datoteci..."
-		// stoga ne usporedjujemo sadrzaj potpisane datoteke sa sazetkom (sto ima smisla jer primatelj vjerojatno 
-		// nece imati pristup originalnoj datoteci) vec ucitavamo datoteku i usporedjujemo njezin sazetak
+		// u zadatku pise: "Kod provjere digitalnog potpisa se očekuje da se provjeri ispravnost digitalnog potpisa 
+		// pri čemu do pogreške (promjene sadržaja) može doći u originalnoj datoteci ili u datoteci koja sadržava 
+		// digitalni potpis. "
+		// stoga ne usporedjujemo samo sadrzaj potpisane datoteke sa nj sazetkom (sto ima smisla jer primatelj vjerojatno 
+		// nece imati pristup originalnoj datoteci) vec usporedjujemo sazetak ucitane originalne datoteke, originalni
+		// sazetak digitalno potpisane datoteke i novokreirani sazetak potpisane datoteke
 		//$org_data = $data_items[0];
 		
 		
@@ -37,11 +40,11 @@
 		
 		
 		$rsa->loadKey($public_key);
-		$signed_digest = $rsa->decrypt($data_items[1]);
+		$signed_org_digest = $rsa->decrypt($data_items[1]);
 		
+		$signed_digest = sha1($data_items[0]);
 		
-		
-		if ($signed_digest == $org_digest) {
+		if ($org_digest == $signed_org_digest && $org_digest == $signed_digest && $signed_org_digest == $signed_digest) {
 			echo "valid";
 		}
 		else {
